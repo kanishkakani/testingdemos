bba334565af337a50d37ea346e7268a5403024ce
kanishkakani <109128177+kanishkakani@users.noreply.github.com>
2022-10-04 15:27:08 +0530

    changes

---
+++ AutomateWebTestng/src/com/test/LoginTest.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	WebDriver driver;
+	@Test
+	public void facebook() {
+		
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("kani@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("kani123");
+		driver.findElement(By.name("login")).submit();
+		
+		
+	}
+	@BeforeMethod
+	public void beforeMethod() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver_win32\\chromedriver.exe");
+		driver= new ChromeDriver();
+	}
+	@AfterMethod
+	public void afterMethod() {
+		//driver.close();
+		driver=null;
+	}
+	
+	
+}
